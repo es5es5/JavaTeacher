d421377280a85694f3249b263a441b3fc329d01e
es5es5 <es5e5s@nate.com>
2018-07-25 15:51:44 +0900

    something change

---
+++ Line/src/com/lhw/line/main/DDD.java
@@ -0,0 +1,48 @@
+package com.lhw.line.main;
+
+
+import java.io.*;
+
+public static class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String line = null;
+		String[] line2 = null;
+		String[] line3 = null;
+		int count = 1;
+		while ((line = br.readLine()) != null) {
+			if (count == 1) {
+				line2 = line.split(":");
+				line3 = line2[1].split("x");
+				for (String string : line3) {
+					string = string.replace(" ", "");
+					System.out.println(Integer.parseInt(string));
+				}
+				count++;
+			} else if (count == 2) {
+				line2 = line.split(":");
+				line2[1] = line2[1].replace(" 0x", "");
+				int r = Integer.parseInt(line2[1], 16);
+				System.out.println(r);
+				count++;
+			} else if (count == 3) {
+				line2 = line.split(":");
+				line2[1] = line2[1].replace(" ", "");
+				System.out.println(line2[1]);
+				line = br.readLine();
+				count++;
+			} else {
+				line2 = line.split(" ");
+				System.out.print(line2[1] + "x" + line2[2] + ", ");
+				System.out.print(line2[4] + ", ");
+				if (line2[3].equals("yes")) {
+					System.out.println("1");
+				} else {
+					System.out.println("0");
+				}
+			}
+
+		}
+	}
+}
